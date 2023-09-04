5b7019f9f7dc5cbdd75f710324a300988b822d2f
ramanbs <bsraman1991@gmail.com>
2023-09-04 20:30:14 +0530

    Added window bg color, showFPS to the title bar and fullscreen mode capability

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #define GLEW_STATIC
 
@@ -10,6 +11,10 @@ const char* APP_TITLE = "Shader Editor";
 const int gWindowWidth = 800;
 const int gWindowHeight = 600;
 
+bool gFullScreen = false;
+
+void glfw_onKeyPressed(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
 
 int main() 
 {
@@ -25,7 +30,24 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	GLFWwindow* pWindow = NULL;
+		
+	if (gFullScreen)
+	{
+		GLFWmonitor* pMonitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* pVmode = glfwGetVideoMode(pMonitor);
+
+		if (pVmode != NULL)
+		{
+			pWindow = glfwCreateWindow(pVmode->width, pVmode->height, APP_TITLE, pMonitor, NULL);
+		}
+	}
+	else
+	{
+		pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	}
+
+	
 
 	if (pWindow == NULL)
 	{
@@ -35,6 +57,8 @@ int main()
 	}
 
 	glfwMakeContextCurrent(pWindow);
+
+	glfwSetKeyCallback(pWindow, glfw_onKeyPressed);
 
 	// For enabling linking modern OpenGL
 	glewExperimental = GL_TRUE;
@@ -50,11 +74,57 @@ int main()
 
 	while (!glfwWindowShouldClose(pWindow)) 
 	{
+		showFPS(pWindow);
 		glfwPollEvents();
+
+		glClearColor(0.23f, 0.30f, 0.47f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(pWindow); 
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+
+void glfw_onKeyPressed(GLFWwindow* window, int key, int scancode, int action, int mode) 
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+void showFPS(GLFWwindow* window) 
+{
+	static double previousSeconds = 0.0f;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // number of seconds since GLFW started, uses high resolution timer
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// limit text update 4 times per second
+
+	if (elapsedSeconds > 0.25f)
+	{
+		previousSeconds = currentSeconds;
+
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		std::ostringstream outStringStream;
+		outStringStream.precision(3);
+
+		outStringStream << std::fixed << APP_TITLE << "  "
+			<< "FPS : " << fps << "  "
+			<< "FrameTime:  " << msPerFrame << " (ms)";
+
+		glfwSetWindowTitle(window, outStringStream.str().c_str());
+
+		frameCount = 0;
+	}
+
+	frameCount++;
 }
